e47da4c72914650717cb912a7807b0d4605afbca
FanX0 <faridazhari46@gmail.com>
2023-05-28 03:19:32 +0700

    Extract Ke dalam Komponen

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import { useState } from 'react';
-import Button from './components/Button';
 import PlaceContentCenter from './components/PlaceContentCenter';
+import Counter from './components/counter';
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    function handleClick() {
-        setCount((prevState) => prevState + 1);
-    }
     return (
         <PlaceContentCenter>
-            <h1 className='text-5xl font-bold'>{count}</h1>
-            <div className='mt-5 flex items-center gap-2'>
-                <Button onClick={handleClick}>Change name</Button>
-                <Button
-                    onClick={() => {
-                        handleClick();
-                        handleClick();
-                        handleClick();
-                    }}>
-                    +3
-                </Button>
-            </div>
+            <Counter initialValue={10} />
         </PlaceContentCenter>
     );
 }
