fd36544e8e51cc64d231967be1324059549d755b
FanX0 <faridazhari46@gmail.com>
2023-05-28 01:46:08 +0700

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import Card from './components/Card';
+import { useState } from 'react';
 import Button from './components/Button';
+import PlaceContentCenter from './components/PlaceContentCenter';
+
 function App() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount((prevState) => prevState + 1);
+    }
     return (
-        <div className='bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'>
-            <div className='max-w-2xl w-full flex items-center gap-4'>
-                <Card>
-                    <Card.Title>Hello React</Card.Title>
-                    <Card.Body>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea accusantium incidunt nulla, mollitia itaque
-                        possimus ad consectetur dolorem minima quaerat nesciunt natus perferendis ratione. Velit aliquid
-                        repellendus nam non corrupti?
-                    </Card.Body>
-
-                    <Card.Footer>
-                        <Button className='bg-red-600'>Click Me</Button>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Title>Hello React</Card.Title>
-                    <Card.Body>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea accusantium incidunt nulla, mollitia itaque
-                        possimus ad consectetur dolorem minima quaerat nesciunt natus perferendis ratione. Velit aliquid
-                        repellendus nam non corrupti?
-                    </Card.Body>
-
-                    <Card.Footer>
-                        <Button className='bg-red-600'>Click Me</Button>
-                    </Card.Footer>
-                </Card>
+        <PlaceContentCenter>
+            <h1 className='text-5xl font-bold'>{count}</h1>
+            <div className='mt-5 flex items-center gap-2'>
+                <Button onClick={handleClick}>Change name</Button>
+                <Button
+                    onClick={() => {
+                        handleClick();
+                        handleClick();
+                        handleClick();
+                    }}>
+                    +3
+                </Button>
             </div>
-        </div>
+        </PlaceContentCenter>
     );
 }
 
