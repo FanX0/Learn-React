206c31ed0be0c13e0398350bb441c6426544fa72
FanX0 <faridazhari46@gmail.com>
2023-05-27 23:22:17 +0700

    add Components + table icons package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
+import {
+    IconBrandFacebook,
+    IconBrandGithub,
+    IconBrandTwitter,
+} from '@tabler/icons-react';
+import clsx from 'clsx';
+
 export default function App() {
     return (
-        <div className='bg-blue-600 grid place-content-center min-h-screen'>
-            <h1 className={'text-xl'}>Learn React</h1>
+        <div className='bg-blue-900 grid place-content-center min-h-screen'>
+            <div className={'flex gap-x-2'}>
+                <Button
+                    {...{
+                        type: 'submit',
+                        onClick: () => console.log('Login with another style'),
+                    }}
+                >
+                    <IconBrandFacebook />
+                    Facebook
+                </Button>
+                <Button onClick={() => console.log(' Facebook')} type='reset'>
+                    <IconBrandFacebook />
+                    Facebook
+                </Button>
+                <Button
+                    className='bg-blue-600'
+                    onClick={() => console.log(' Twitter')}
+                    type='submit'
+                >
+                    <IconBrandTwitter />
+                    Twitter
+                </Button>
+                <Button
+                    className='bg-black'
+                    onClick={() => console.log(' Github')}
+                    type='submit'
+                >
+                    <IconBrandGithub />
+                    Github
+                </Button>
+            </div>
         </div>
+    );
+}
+
+function Button(props) {
+    const { className = 'bg-red-600', children, text, type = 'submit' } = props;
+    return (
+        <button
+            {...props}
+            className={clsx(
+                className,
+                `[&>svg]:w-5 [&>svg]:h-5  [&>svg]:stroke-1 flex item-center gap-x-2  text-white px-4 py-2 rounded`
+            )}
+        >
+            {text || children}
+        </button>
     );
 }
