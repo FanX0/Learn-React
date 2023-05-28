7fb2d6501243a84ad495c3edebcc7fddf980c551
FanX0 <faridazhari46@gmail.com>
2023-05-28 18:45:16 +0700

    Add Form useState dengan Object

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
-import { useState } from 'react';
 import PlaceContentCenter from './components/PlaceContentCenter';
-import Counter from './components/counter';
+import Card from './components/Card';
+import Button from './components/Button';
+import Input from './components/Input';
+import Label from './components/Label';
+import { IconBrandGithub } from '@tabler/icons-react';
+import { useState } from 'react';
 
 function App() {
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+    });
+
+    function onChange(event) {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    }
+
+    function submit(event) {
+        event.preventDefault();
+
+        console.log(form);
+    }
+
     return (
         <PlaceContentCenter>
-            <Counter initialValue={10} />
+            <Card>
+                <div className='mb-5 border rounded-lg p-4'>
+                    <p>Name {form.name || '---'}</p>
+                    <p>Email {form.email || '---'}</p>
+                </div>
+                <Card.Title>Sign up for new account!</Card.Title>
+                <form onSubmit={submit}>
+                    {' '}
+                    <Card.Body>
+                        <div className='mb-6'>
+                            <Label htmlFor='name' value={'Name'}></Label>
+                            <Input value={form.name} onChange={onChange} id={'name'} name={'name'} />
+                        </div>
+                        <div className='mb-6'>
+                            <Label htmlFor='email' value={'Email'}></Label>
+                            <Input value={form.email} onChange={onChange} id={'email'} name={'email'} />
+                        </div>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button>
+                            <IconBrandGithub />
+                        </Button>
+                    </Card.Footer>
+                </form>
+            </Card>
         </PlaceContentCenter>
     );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 function Card({ children }) {
-    return <div className='shadow rounded-lg overflow-hidden bg-white'>{children}</div>;
+    return <div className='shadow rounded-lg overflow-hidden bg-white text-black'>{children}</div>;
 }
 
 function Title({ children }) {
     return (
         <div className={' p-4 border-b'}>
-            <h1 className={'text-xl'}> {children}</h1>
+            <h1 className={'text-xl '}> {children}</h1>
         </div>
     );
 }
